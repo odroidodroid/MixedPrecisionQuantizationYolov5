d39f9d411c90cc8d9e425b920b10e889ae3175e9
root <zjzlem200@gmail.com>
2022-05-18 16:38:15 +0900

    second upload

---
+++ QAT/prepare_model.py
@@ -0,0 +1,15 @@
+import models.yolo
+import models.common
+
+def forward_hook () :
+    if True :
+        pass
+
+def backward_hook () :
+    if True :
+        pass
+
+def prepare_model () :
+    if True :
+        pass
+
